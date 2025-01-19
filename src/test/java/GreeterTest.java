eea7c2704feae9db7fe079e05d6211990b6345fe
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2025-01-19 13:26:32 +0200

    Requirement 4: add multiple values for test

--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,5 +1,7 @@
 import org.example.Greeter;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalTime;
 
@@ -50,10 +52,19 @@ public class GreeterTest {
     }
 
 
-    @Test
-    void shouldReturnGoodMorningWhenTimeIs06_00_To_12_00() {
+    @ParameterizedTest
+    @CsvSource({
+            "6, 0",
+            "7, 15",
+            "8, 30",
+            "9, 45",
+            "10, 50",
+            "11, 59",
+            "12, 0"
+    })
+    void shouldReturnGoodMorningWhenTimeIs06_00_To_12_00(int hour, int minute) {
 //        Given
-        Greeter greeter = new Greeter(LocalTime.of(8, 0));
+        Greeter greeter = new Greeter(LocalTime.of(hour, minute));
         String name = "peter";
 
 //        When
