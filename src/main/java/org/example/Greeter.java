24a773195c298584a0b3247279e37fc29fc73250
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2025-01-19 13:17:42 +0200

    Requirement 4: returns good morning <name> when the time is 6:00 - 12:00

--- src/main/java/org/example/Greeter.java
+++ src/main/java/org/example/Greeter.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.time.LocalTime;
+
 public class Greeter {
 
+    private LocalTime localTime;
+
+    public Greeter() {
+        this.localTime = LocalTime.now();
+    }
+
+    public Greeter(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
     public String greet(String input) {
+        var hour = this.localTime.getHour();
+        if (hour >= 6 && hour <= 12) {
+            return "Good morning " + sanitize(input);
+        }
         return "Hello " + sanitize(input);
     }
 
--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,6 +1,8 @@
 import org.example.Greeter;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreeterTest {
@@ -48,5 +50,18 @@ public class GreeterTest {
     }
 
 
+    @Test
+    void shouldReturnGoodMorningWhenTimeIs06_00_To_12_00() {
+//        Given
+        Greeter greeter = new Greeter(LocalTime.of(8, 0));
+        String name = "peter";
+
+//        When
+        String result = greeter.greet(name);
+
+//        Then
+        String expected = "Good morning Peter";
+        assertEquals(expected, result);
+    }
 
 }
